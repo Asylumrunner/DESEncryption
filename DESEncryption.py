684284a40dc759ef0ef1c148f9b3ed51aa93cd69
Asylumrunner <michael@mpburdick.com>
2017-03-01 08:27:39 -0600

    Completed DES algorithm

--- DESEncryption.py
+++ DESEncryption.py
@@ -2,19 +2,6 @@ __author__ = 'Michael Burdick'
 
 import sys
 
-
-def permute(list_to_permute, permutation_matrix):
-    permuted_list = [] #create an empty list
-    for x in range(len(permutation_matrix)): #for every value in the matrix
-        permuted_list.append(list_to_permute[permutation_matrix[x]]) #use the permutation matrix to see what value should be placed here
-    return permuted_list
-
-
-def permute_key(key, permutation_matrix_C, permuation_matrix_D):
-    key_permutations = []
-    
-
-
 initial_permutation = [58, 50, 42, 34, 26, 18, 10, 2,
                        60, 52, 44, 36, 28, 20, 12, 4,
                        62, 54, 46, 38, 30, 22, 14, 6,
@@ -51,15 +38,24 @@ thirty_two_bit_permutation = [16, 7, 20, 21,
                               19, 13, 30, 6,
                               22, 11, 4, 25]
 
-key_permutations_C = [57, 49, 41, 33, 25, 17, 9,
-                      1, 58, 50, 42, 34, 26, 18,
-                      10, 2, 59, 51, 43, 35, 27,
-                      19, 11, 3, 60, 52, 44, 36]
+key_permutations_1_C = [57, 49, 41, 33, 25, 17, 9,
+                        1, 58, 50, 42, 34, 26, 18,
+                        10, 2, 59, 51, 43, 35, 27,
+                        19, 11, 3, 60, 52, 44, 36]
 
-key_permutations_D = [63, 55, 47, 39, 31, 23, 15,
-                      7, 62, 54, 46, 38, 30, 22,
-                      14, 6, 61, 53, 45, 37, 29,
-                      21, 13, 5, 28, 20, 12, 4]
+key_permutations_1_D = [63, 55, 47, 39, 31, 23, 15,
+                        7, 62, 54, 46, 38, 30, 22,
+                        14, 6, 61, 53, 45, 37, 29,
+                        21, 13, 5, 28, 20, 12, 4]
+
+permuted_choice_2 = [14, 17, 11, 24, 1, 5,
+                     3, 28, 15, 6, 21, 10,
+                     23, 19, 12, 4, 26, 8,
+                     16, 7, 27, 20, 13, 2,
+                     41, 52, 31, 37, 47, 55,
+                     30, 40, 51, 45, 33, 48,
+                     44, 49, 39, 56, 34, 53,
+                     46, 42, 50, 36, 29, 32]
 
 S1_row_one = [14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7]
 S1_row_two = [0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8 ]
@@ -109,25 +105,137 @@ S8_row_three = [7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8]
 S8_row_four = [2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11]
 S8 = [S8_row_one, S8_row_two, S8_row_three, S8_row_four]
 
+
+def permute(list_to_permute, permutation_matrix): #permute takes a list and permutes it according to a matrix
+    permuted_list = [] #create an empty list
+    for x in range(len(permutation_matrix)): #for every value in the matrix
+        permuted_list.append(list_to_permute[permutation_matrix[x]-1]) #use the permutation matrix to see what value should be placed here
+    return permuted_list #of note is the fact that permute can make shorter lists longer, which we use for our f function
+
+
+def permute_key(key, permutation_matrix_C, permuation_matrix_D): #permute_key takes in an initial key, and two halves of the key permutation matrix, and outputs all keys for encryption/decryption
+    key_permutations = []#create an empty list to hold all the keys
+    key_in_list_form = list(str(key))#turn the key into a list
+    key_C = permute(key_in_list_form, permutation_matrix_C)#permute the left half
+    key_D = permute(key_in_list_form, permuation_matrix_D)#permute the right half
+    two_left_shifts = [3, 4, 5, 6, 7, 8, 10, 11, 12, 13, 14, 15]#mark the iterations where we have to left shift twice
+    for x in range(16):#then, for sixteen iterations
+        if two_left_shifts.count(x) == 1:
+            temp_C = key_C.pop(0)
+            temp_D = key_D.pop(0)
+            key_C.append(temp_C)
+            key_D.append(temp_D)
+
+        temp_C = key_C.pop(0)#perform left shifts as needed
+        temp_D = key_D.pop(0)
+        key_C.append(temp_C)
+        key_D.append(temp_D)
+        key_permutations.append(permute(key_C+key_D, permuted_choice_2))#then use PC2 on that shifted keystring to generate this round's subkey
+
+    return key_permutations #then return the list of subkeys
+
+
+def s_function(block, s_chart):#the s function is simple
+    column = (block[1]*8) + (block[2]*4) + (block[3]*2) + block[4]#it picks the column equal to the four-digit binary number demonstrated by the middle four numbers of the block
+    row = (block[0]*2) + block[5]#and the column designated by the two digit binary nummber demonstrated by the first and last digits
+    return bin(s_chart[row][column])[2:].zfill(4)#then it returns the result as a four-bit binary number
+
+
+def f_function(text, key): #the f-function is the meat of the encryption/decryption process
+    extended_text = permute(text, E_bit_selection_table)#first, use permute to extend the plaintext to 48 bits
+    text_xored_with_data = []
+    for x in range(48):#then, xor the text with the key
+        text_xored_with_data.append(int(extended_text[x]) ^ int(key[x]))
+    block_S1 = text_xored_with_data[0:6]#split this result into 6-bit blocks
+    block_S2 = text_xored_with_data[6:12]
+    block_S3 = text_xored_with_data[12:18]
+    block_S4 = text_xored_with_data[18:24]
+    block_S5 = text_xored_with_data[24:30]
+    block_S6 = text_xored_with_data[30:36]
+    block_S7 = text_xored_with_data[36:42]
+    block_S8 = text_xored_with_data[42:]
+
+    s1_resolved = str(s_function(block_S1, S1))#then, apply the s-function to each of these 6-bit blocks
+    s2_resolved = str(s_function(block_S2, S2))
+    s3_resolved = str(s_function(block_S3, S3))
+    s4_resolved = str(s_function(block_S4, S4))
+    s5_resolved = str(s_function(block_S5, S5))
+    s6_resolved = str(s_function(block_S6, S6))
+    s7_resolved = str(s_function(block_S7, S7))
+    s8_resolved = str(s_function(block_S8, S8))
+
+    pre_p_permutation = s1_resolved + s2_resolved + s3_resolved + s4_resolved + s5_resolved + s6_resolved + s7_resolved + s8_resolved#append all of these blocks together
+    pre_p_permuation_list = list(pre_p_permutation)#then convert them to a list
+    return permute(pre_p_permuation_list, thirty_two_bit_permutation)#lastly, return the result of permuting that list by the 32-bit permutation
+
+
 #------------START OF MAIN---------------------
 plaintext_filename = sys.argv[1]
+if sys.argv[2] == "encrypt": #a sysarg determines if you're encrypting or decrypting
+    encrypt = True
+else:
+    encrypt = False
 plaintext_file = open(plaintext_filename, "r")
 plaintext = []
 
 for line in plaintext_file:
     plaintext.append(line)
 
-plaintext_file.close()
-key = 1001011010010110100101101001011010010110100101101001011010010110
+plaintext_file.close()#open the file, read in the data to be processed, then close it
+key = 1001011010010110100101101001011010010110100101101001011010010110 #I'm hardcoding in the key rn
 
-plaintext_bin = plaintext[0].split()
+plaintext_bin = list(plaintext[0])
 
-permuted_plaintext = permute(plaintext_bin, initial_permutation)
+if encrypt:#if we're encrypting
+    permuted_plaintext = permute(plaintext_bin, initial_permutation)#perform the initial permutation upon the plaintext
 
-L = permuted_plaintext[0:32]
-R = permuted_plaintext[32:]
+    L = permuted_plaintext[0:32]#split the plaintext into two blocks, L and R
+    R = permuted_plaintext[32:]
 
-key_permutations = permute_key(key, key_permutations_C, key_permutations_D)
+    key_permutations = permute_key(key, key_permutations_1_C, key_permutations_1_D) #generate all key permutations we need
 
+    for x in range(16): #for sixteen rounds
+        new_L = R #L = R
+        f_r_k = f_function(R, key_permutations[x])
+        new_R = []
+        for y in range(32):
+            new_R.append(int(L[y]) ^ int(f_r_k[y])) #R = L XOR f(R, K)
+        L = new_L
+        R = new_R
 
+    preoutput = L + R
 
+    output = permute(preoutput, inverse_initial_permutations)#then, perform the inverse initial permutation
+
+    outfile = open("outfile.txt", "w")#write it to a file
+    for element in output:
+        outfile.write(str(element))
+    outfile.write('\n')
+    outfile.close()
+
+else:
+    permuted_plaintext = permute(plaintext_bin, initial_permutation)
+
+    L = permuted_plaintext[0:32]
+    R = permuted_plaintext[32:]
+
+    key_permutations = permute_key(key, key_permutations_1_C, key_permutations_1_D)
+
+    for x in range(16):
+        new_R = L
+        f_r_k = f_function(L, key_permutations[15-x])
+        new_L = []
+        for y in range(32):
+            new_L.append(int(R[y]) ^ int(f_r_k[y]))
+        L = new_L
+        R = new_R
+
+    preoutput = L + R
+
+    output = permute(preoutput, inverse_initial_permutations)
+
+    outfile = open("decryptedOutfile.txt", "w")
+    for element in output:
+        outfile.write(str(element))
+    outfile.write('\n')
+    outfile.close()
